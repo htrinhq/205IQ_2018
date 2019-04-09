403975a7f817b50165102d78c9c53ea1e2d5eee7
Hugo TRINH <hugo.trinh-quy@epitech.eu>
2019-04-09 12:03:14 +0200

    add computation for 2 args.

--- IQ.py
+++ IQ.py
@@ -10,13 +10,19 @@ from math import pi, sqrt, exp
 def get_proba(data: [int]) -> [float]:
     """Get the proba result tab, from 0 to 200."""
     IQ_proba = []
-    # result = 0.00
     for i in range (0, 201):
-        result = 1 / (data[1] * sqrt(2 * pi)) * exp(- (pow((i - data[0]), 2) / pow(2 * data[1], 2)))
-        IQ_proba.append(result * 100)
+        result = 1 / (data[1] * sqrt(2 * pi)) * \
+            exp(-0.5 * pow((i - data[0]) / data[1], 2))
+        IQ_proba.append(result)
     return IQ_proba
 
+def print_IQ_tab(IQ_tab: [float]) -> None:
+    """Print proba results from 0 to 200."""
+    for i in range(0, 201):
+        print('{:d} {:.05f}' .format(i, IQ_tab[i]))
 
 def IQ_parser(data: [int]) -> None:
     """Redirect to correct IQ calculator option."""
-    print(len(get_proba(data)))
+    IQ_tab = get_proba(data)
+    if len(data) == 2:
+        print_IQ_tab(IQ_tab)
