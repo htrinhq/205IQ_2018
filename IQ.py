a52584b40e505c96020688a3f0b0c2d6c1f6ca15
Hugo TRINH <hugo.trinh-quy@epitech.eu>
2019-04-09 09:57:08 +0200

    get proba function from 0 to 200

--- IQ.py
+++ IQ.py
@@ -5,6 +5,18 @@
 ## IQ
 ##
 
+from math import pi, sqrt, exp
+
+def get_proba(data: [int]) -> [float]:
+    """Get the proba result tab, from 0 to 200."""
+    IQ_proba = []
+    # result = 0.00
+    for i in range (0, 201):
+        result = 1 / (data[1] * sqrt(2 * pi)) * exp(- (pow((i - data[0]), 2) / pow(2 * data[1], 2)))
+        IQ_proba.append(result * 100)
+    return IQ_proba
+
+
 def IQ_parser(data: [int]) -> None:
     """Redirect to correct IQ calculator option."""
-    pass
+    print(len(get_proba(data)))
